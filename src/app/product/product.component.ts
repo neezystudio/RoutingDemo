383e5f2de4eddeaf3a4eef85e3457a9e7f997677
neezystudio <victor.ndirangu@kyosk.app>
2024-03-04 14:51:38 +0300

    wip

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
+  productDetail? : any;
 
+  constructor(private route : ActivatedRoute,private productService : ProductsService){}
+
+  ngOnInit(): void {
+    let productId = this.route.snapshot.params['id'];
+    this.getProductDetailById(productId)
+  }
+
+  getProductDetailById(id: number){
+    this.productService.getProductDetailById(id).subscribe((res: any) => {
+      this.productDetail = res
+      console.log(res)
+    })
+  }
+  
 }
+
