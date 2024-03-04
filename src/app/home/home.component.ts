f5eaaa80d31ceac0b0f1383a34c29fedebb5d7f3
neezystudio <victor.ndirangu@kyosk.app>
2024-03-04 16:46:38 +0300

    home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
+import { ProductsService } from 'src/services/products.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+// @NgModule({
+//   declarations: [
+//     // List of components, directives, and pipes used in this module
+//   ],
+//   imports: [
+//     // List of modules imported by this module
+//   ],
+//   providers: [
+//     // List of services provided by this module
+//   ]
+// })
+export class HomeComponent implements OnInit{
+  constructor(private productService : ProductsService){}
 
+  productList : any;
+
+  ngOnInit(){
+    this.getProduct()
+  }
+
+  getProduct(){
+    this.productService.getProductList().subscribe(res =>{
+      this.productList = res
+      console.log(this.productList);
+    })
+  }
 }
