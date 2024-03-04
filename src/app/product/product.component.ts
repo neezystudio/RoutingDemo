fcf2c9ebfa6dd8b6cd1288041e1a4ea2f81fe950
neezystudio <victor.ndirangu@kyosk.app>
2024-03-04 15:48:09 +0300

    fake product service from fake api

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from 'src/services/products.service';
 
 @Component({
   selector: 'app-product',
