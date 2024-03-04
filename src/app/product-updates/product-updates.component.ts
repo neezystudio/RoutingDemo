fc15284699ac78fbc873c0bc6097bb7eacb474d9
neezystudio <victor.ndirangu@kyosk.app>
2024-03-04 11:28:16 +0300

    wip

--- src/app/product-updates/product-updates.component.ts
+++ src/app/product-updates/product-updates.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-product-updates',
   templateUrl: './product-updates.component.html',
-  styleUrls: ['./product-updates.component.css']
+  styleUrls: ['./product-updates.component.scss']
 })
 export class ProductUpdatesComponent {
 
