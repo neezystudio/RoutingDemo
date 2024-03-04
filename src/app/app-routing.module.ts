66954ad175ae4486b0d782a9980e35a227caa3d4
neezystudio <victor.ndirangu@kyosk.app>
2024-03-04 12:41:11 +0300

    app routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { HomeComponent } from './home/home.component';
+import { ProductOfferComponent } from './product-offer/product-offer.component';
+import { ProductUpdatesComponent } from './product-updates/product-updates.component';
+import { ProductComponent } from './product/product.component';
+import { RatingComponent } from './rating/rating.component';
 
 const routes: Routes = [
-
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, //default route
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'product/:id', component: ProductComponent,
+    children: [
+      {path: '', redirectTo:'updates', pathMatch:'full'},
+      { path: 'offers', component: ProductOfferComponent },
+      { path: 'updates', component: ProductUpdatesComponent }
+    ]
+  },
+  { path: 'about', component: AboutComponent, 
+    children: [
+      {path: 'rating', outlet:'rate', component:RatingComponent},
+      {path: 'feedback', outlet:'feed', component:FeedbackComponent}
+    ]
+  },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', component: HomeComponent }
 ];
 
 @NgModule({
