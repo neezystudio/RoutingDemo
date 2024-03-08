b6ffda547ad6f458a52a3bb66ac59c6ad91d4eed
neezystudio <victor.ndirangu@kyosk.app>
2024-03-08 11:32:57 +0300

    profile form wip

--- src/app/profile-forms/profile-forms.component.ts
+++ src/app/profile-forms/profile-forms.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IonicModule} from '@ionic/angular';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-forms',
   templateUrl: './profile-forms.component.html',
-  styleUrls: ['./profile-forms.component.scss']
+  styleUrls: ['./profile-forms.component.scss'],
+  imports:[IonicModule],
 })
-export class ProfileFormsComponent {
+export class ProfileFormsComponent  {
+  profileform!: FormGroup;
+  ngOnInit(){
+    this.profileform = new FormGroup({
+      'name': new FormControl(null, Validators.required),
+    })
+  }
 
 }
